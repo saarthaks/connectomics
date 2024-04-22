78bfb246fbda4db25170c3c480e60d0066abd0d5
saarthaks <saarthak.sarup@gmail.com>
2024-04-22 14:19:53 -0700

    error checking

--- core/flywire_utils.py
+++ core/flywire_utils.py
@@ -9,28 +9,39 @@ from core.GenericBranch import GenericBranchSeq
 from core.Tree import Tree
 
 def load_tree(neuron_id, datapath='/drive_sdc/ssarup/flywire_data/all_trees'):
-    with open(os.path.join(datapath, f'{neuron_id}.pkl'), 'rb') as f:
-        tree = pickle.load(f)
-    return tree
-    
+    try:
+        with open(os.path.join(datapath, f'{neuron_id}.pkl'), 'rb') as f:
+            tree = pickle.load(f)
+            return tree
+    except:
+        print(f'Error loading {neuron_id}')
+        return None    
 
 def load_tree_dict(neuron_ids, datapath='/drive_sdc/ssarup/flywire_data/all_trees'):
     neuron_ids = list(set(neuron_ids))
 
     trees = {}
     for neuron_id in tqdm(neuron_ids):
-        trees[neuron_id] = load_tree(neuron_id, datapath=datapath)
+        tree = load_tree(neuron_id, datapath=datapath)
+        if tree is not None:
+            trees[neuron_id] = tree
     return trees
 
 def load_branches(neuron_id, datapath='/drive_sdc/ssarup/flywire_data/all_branches'):
-    with open(os.path.join(datapath, f'{neuron_id}.pkl'), 'rb') as f:
-        branches = pickle.load(f)
-    return branches
+    try:
+        with open(os.path.join(datapath, f'{neuron_id}.pkl'), 'rb') as f:
+            branches = pickle.load(f)
+            return branches
+    except:
+        print(f'Error loading {neuron_id}')
+        return None
 
 def load_branches_dict(neuron_ids, datapath='/drive_sdc/ssarup/flywire_data/all_branches'):
     neuron_ids = list(set(neuron_ids))
 
     branches = {}
     for neuron_id in tqdm(neuron_ids):
-        branches[neuron_id] = load_branches(neuron_id, datapath=datapath)
+        branch = load_branches(neuron_id, datapath=datapath)
+        if branch is not None:
+            branches[neuron_id] = branch
     return branches
